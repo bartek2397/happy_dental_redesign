458d1e83b35d4eb56556bc5659b1d283da3a8d39
bartek9723 <bartosz.wszolek97@gmail.com>
2023-07-06 13:19:12 +0200

    working on user auth

--- app/components/inputs/FormInput.tsx
+++ app/components/inputs/FormInput.tsx
@@ -1,17 +1,39 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React from "react";
+
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface FormInputProps {
+  id: string;
   type: string;
-  className?: string
+  className?: string;
   placeholder?: string;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
 }
 
-const FormInput: React.FC<FormInputProps> = ({ type = 'text', className, placeholder }) => {
+const FormInput: React.FC<FormInputProps> = ({
+  id,
+  type = "text",
+  className,
+  placeholder,
+  required,
+  register,
+  errors,
+}) => {
   return (
-    <input type={type} className={`border border-solid-1px border-gray-300 ${className ?? ''}`} placeholder={placeholder}/>
-  )
-}
+    <input
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      {...register(id, { required })}
+      className={`border border-solid-1px border-gray-300 ${className ?? ""} ${
+        errors[id] ? "border-rose-500" : "border-gray-300"
+      }`}
+    />
+  );
+};
 
 export default FormInput;
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -7,29 +7,24 @@ import { useForm, SubmitHandler, FieldValues } from 'react-hook-form'
 
 import axios from "axios";
 
-const {
-  register,
-  handleSubmit,
-  formState: {
-    errors,
-  },
-} = useForm<FieldValues>({
-  defaultValues: {
-    email: '',
-    password: '',
-    confirmPassword: ''
-  }
-})
 
-interface IFormInput {
-  email: string;
-  password: string;
-  confirmPassword: string
-}
 
 const SignUp = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors,
+    },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  })
+  
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
     axios.post('/api/register', data).then(() => {
       
     })
@@ -38,11 +33,11 @@ const SignUp = () => {
     <div className='max-w-full h-[75vh] px-4'>
       <div className='max-w-[1440px] h-[75%] mx-auto text-center relative'>
         <div className="w-[75%] lg:w-[50%] bg-[#e4eee3] rounded-lg border border-solid-1px border-black absolute left-[50%] -translate-x-[50%] top-[20%] ">
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 px-4 py-4">
-            <FormInput type='email' placeholder="Email" className=""/>
-            <FormInput type='password' placeholder="Password" className="" />
-            <FormInput type='password' placeholder='Confirm Password' className="" />
-            <Button label="Submit" color="#C5D8DF" className=" py-4" />
+          <form className="flex flex-col gap-4 px-4 py-4">
+            <FormInput id="email" type='email' placeholder="Email" errors={errors} register={register} required/>
+            <FormInput id="password" type='password' placeholder="Password" errors={errors} register={register} required />
+            <FormInput id="confirmPassword" type='password' placeholder='Confirm Password' errors={errors} register={register} required/>
+            <Button onClick={handleSubmit(onSubmit)} label="Submit" color="#C5D8DF" className=" py-4" />
           </form>
         </div>
       </div>
