f87188ed5ded03613a26789f31d25c5758d82455
bartek2397 <bartosz.wszolek97@gmail.com>
2023-07-23 09:27:37 +0200

    404 error page

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -14,17 +14,21 @@ interface EmptyStateProps {
 
 const EmptyState: React.FC<EmptyStateProps> = ({ title, subtitle }) => {
   return (
-    <div className="bg-background relative">
-        {/* <Image
+    <div className="w-full h-[86vh] bg-background relative">
+        <div className="max-w-[1440px] m-auto h-full relative px-4">
+          <Image
             src={background}
             alt="Background"
             className="absolute bottom-[0%] right-[50%] translate-x-[50%] w-[80%] max-h-[90vh] "
-          /> */}
-          <div className="absolute bg-white backdrop-blur-sm z-10">
-            <Image src={shield} alt="Error" />
-            <Heading title='404' className="text-[200px]" />
-            <div>{title}</div>
+          />
+          <div className="absolute top-3 left-[50%] -translate-x-[50%] w-[1280px] h-[735px]  text-center bg-[#CECECE]/70 backdrop-blur-md drop-shadow-xl rounded-xl z-10 p-24">
+            <div className="flex items-center justify-around text-[200px] font-heading font-semibold text-[#463C3C]">
+              <Image src={shield} alt="Error" />
+              <Heading title='404' center className="text-[200px]" />
+            </div>
+            <div className="text-center text-[64px] uppercase font-semibold font-heading text-white">{title}</div>
           </div>
+        </div>
     </div>
   )
 }
---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+"use client";
+
+import EmptyState from "./components/EmptyState";
+
+const ErrorState = () => {
+  return (
+      <EmptyState title="This page doesn't exist!" />
+  );
+};
+
+export default ErrorState;
